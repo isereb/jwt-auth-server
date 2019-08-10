5eb0cff22bc40529ac2d8b007e9a474c1dcb4d99
isereb <gWb4P5NLhqHre2f>
2019-08-10 14:52:31 +0000

    Changed log level and message in case when auth failes

--- src/main/java/com/example/jwt/authserver/controller/JwtRequestFilter.java
+++ src/main/java/com/example/jwt/authserver/controller/JwtRequestFilter.java
@@ -46,7 +46,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
                 System.out.println("JWT Token has expired");
             }
         } else {
-            logger.warn("JWT Token does not begin with Bearer String");
+            logger.trace("JWT Token is missing or does not begin with Bearer");
         }
         // Once we get the token validate it.
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
