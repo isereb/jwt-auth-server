4cb58ab5874573580e5f214815b40a6507964ce9
isereb <gWb4P5NLhqHre2f>
2019-08-10 14:52:09 +0000

    DTO Code cleanup, removed redundant code

--- src/main/java/com/example/jwt/authserver/dto/JwtRequest.java
+++ src/main/java/com/example/jwt/authserver/dto/JwtRequest.java
@@ -1,11 +1,9 @@
 package com.example.jwt.authserver.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Value;
 
 @Value
-@AllArgsConstructor
 public class JwtRequest {
-    private String username;
-    private String password;
+    String username;
+    String password;
 }
--- src/main/java/com/example/jwt/authserver/dto/JwtResponse.java
+++ src/main/java/com/example/jwt/authserver/dto/JwtResponse.java
@@ -1,10 +1,8 @@
 package com.example.jwt.authserver.dto;
 
-import lombok.RequiredArgsConstructor;
 import lombok.Value;
 
 @Value
-@RequiredArgsConstructor
 public class JwtResponse {
     String token;
 }
